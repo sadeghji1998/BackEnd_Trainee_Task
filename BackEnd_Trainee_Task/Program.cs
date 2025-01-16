7de73e6915f5d3bbaa09dd2dc209dd67d99fe06c
Sadegh Jafari <sadeghji1998@gmail.com>
2025-01-16 17:54:52 +0330

    Add project files.

---
+++ BackEnd_Trainee_Task/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd_Trainee_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
